ece55550aae4d2e5b427f161725cc53f6738b53f
Ishankulov <ishankulov7m@gmail.com>
2023-05-16 16:16:59 +0500

    Displaying platforms icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/react.svg";
+import logo from "../assets/logo.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
     <>
       <HStack justifyContent="space-between" padding="10px">
-        <Image src={logo} boxSize="50px" />
+        <Image src={logo} width="100px" height='60px' ml={{lg:"50px"}}/>
         <ColorModeSwitch/>
       </HStack>
     </>

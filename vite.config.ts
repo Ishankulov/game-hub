d33842a3d0d3cba7fa24bd50a8facea84478a46d
Ishankulov <ishankulov7m@gmail.com>
2023-05-22 19:26:09 +0500

    Fix bugs to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['/assets/index-f52e0bb2.js'],
-    },
-  },
   plugins: [react()],
 })
